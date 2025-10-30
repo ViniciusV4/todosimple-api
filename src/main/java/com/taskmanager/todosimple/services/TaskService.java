01ad426e09433cc5eb8ad3500aee4e1dd8d82a85
Marcos <marcos.vinicius59533@gmail.com>
2025-10-30 20:47:25 -0300

    add findAllByUserId method to TaskService for retrieving tasks by user ID

--- src/main/java/com/taskmanager/todosimple/services/TaskService.java
+++ src/main/java/com/taskmanager/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.taskmanager.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,11 @@ public class TaskService {
     public Task findById(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
         return task.orElseThrow(() -> new RuntimeException("Task not found id: " + id + ", Type: " + Task.class.getName()));
+    }
+
+    public List<Task> findAllByUserId(Long userId) {
+        List<Task> tasks = this.taskRepository.findByUserId(userId);
+        return tasks;
     }
 
     @Transactional
