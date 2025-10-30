8278a0e7fbeb2c70c8d70ce90d71b6935de10680
Marcos <marcos.vinicius59533@gmail.com>
2025-10-30 20:47:32 -0300

    add findByUserId method to TaskRepository for retrieving tasks by user ID

--- src/main/java/com/taskmanager/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/taskmanager/todosimple/repositories/TaskRepository.java
@@ -1,9 +1,11 @@
 package com.taskmanager.todosimple.repositories;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.taskmanager.todosimple.models.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-
+    List<Task> findByUserId(Long userId);
 }
