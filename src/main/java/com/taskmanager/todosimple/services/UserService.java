e38cd8a49dcc429623706662e11ce369472c819c
Marcos <marcos.vinicius59533@gmail.com>
2025-10-30 14:35:00 -0300

    remove unused TaskRepository dependency from UserService

--- src/main/java/com/taskmanager/todosimple/services/UserService.java
+++ src/main/java/com/taskmanager/todosimple/services/UserService.java
@@ -15,9 +15,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException("User not found id: " + id + ", Type: " + User.class.getName()));
@@ -27,7 +24,6 @@ public class UserService {
     public User create(User user) {
         user.setId(null);
         user = this.userRepository.save(user);
-        this.taskRepository.saveAll(user.getTasks());
         return user;
     }
 
