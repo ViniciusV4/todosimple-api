2c34ffaeae9f007169885b06008c63aeeed03000
Marcos <marcos.vinicius59533@gmail.com>
2025-10-30 16:38:55 -0300

    remove unused import for TaskRepository in UserService

--- src/main/java/com/taskmanager/todosimple/services/UserService.java
+++ src/main/java/com/taskmanager/todosimple/services/UserService.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.taskmanager.todosimple.repositories.TaskRepository;
 import com.taskmanager.todosimple.repositories.UserRepository;
 
 import com.taskmanager.todosimple.models.User;
