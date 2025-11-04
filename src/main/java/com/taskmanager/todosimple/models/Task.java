09ade599a2157fdb102ba20dc2826fd14298bef5
Marcos <marcos.vinicius59533@gmail.com>
2025-11-04 11:30:22 -0300

    refactor Task class to use Lombok annotations for boilerplate reduction

--- src/main/java/com/taskmanager/todosimple/models/Task.java
+++ src/main/java/com/taskmanager/todosimple/models/Task.java
@@ -11,9 +11,19 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Task {
     public static final String TABLE_NAME = "tasks";
 
@@ -32,73 +42,7 @@ public class Task {
     @Size(max = 255)
     private String description;
 
-    public Task() {
-    }
+ 
 
-    public Task(Long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    //getters and setters
-    public Long getId() {   
-        return id;
-    }           
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Task description (String description) {
-        setDescription(description);
-        return this;
-    }
-
-    public Task user (User user) {
-        setUser(user);
-        return this;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Task other = (Task) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
 
 }
